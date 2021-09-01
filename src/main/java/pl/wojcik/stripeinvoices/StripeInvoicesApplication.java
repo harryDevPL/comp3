d05e062f3415a7612d4796052c10ce576e1ba0ec
Krzysztof Wójcik <kristricity@gmail.com>
2021-09-01 08:54:10 +0200

    initial commit with default packages

---
+++ src/main/java/pl/wojcik/stripeinvoices/StripeInvoicesApplication.java
@@ -0,0 +1,13 @@
+package pl.wojcik.stripeinvoices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StripeInvoicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StripeInvoicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/wojcik/stripeinvoices/StripeInvoicesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wojcik.stripeinvoices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StripeInvoicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
